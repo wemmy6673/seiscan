c49903e7eccf4bbb799ee322909d17a9ea19bcbf
wemmy6673 <okunolawemzy@gmail.com>
2025-07-14 23:14:27 +0100

    landing page plus animation

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -14,11 +14,11 @@ const features = [
 
 const FeaturesSection = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-6 m-6 my-14 md:m-14 max-w-2xl md:mx-6 lg:mx-auto md:max-w-6xl">
+    <div className="flex flex-col md:flex-row gap-6 max-w-5xl mx-auto px-6 py-12" data-aos="fade-up" data-aos-duration="1200">
       {features.map((feature, idx) => (
         <div
           key={idx}
-          className="flex-1 bg-gray-800 rounded-xl shadow-md px-4 py-8 text-white text-center text-lg font-medium"
+          className="flex-1 bg-gray-800 rounded-xl shadow-md p-8 text-white text-center text-lg font-medium"
         >
           {feature.title}
         </div>
